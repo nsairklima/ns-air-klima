c1db69a87e4dd07a81bcb08bcf3ce7193a742324
nsairklima <nsair.klima@gmail.com>
2026-03-10 15:11:17 +0100

    Create page.tsx

---
+++ prisma/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>NS-AIR KLÍMA rendszer</h1>
+      <p>Üdvözlöm! A rendszer hamarosan teljes funkcionalitással működni fog.</p>
+
+      <ul>
+        <li>Ajánlatkészítés</li>
+        <li>Karbantartás nyilvántartás</li>
+        <li>Ügyfélkezelés</li>
+        <li>Automatikus email értesítések</li>
+      </ul>
+    </div>
+  );
+}
