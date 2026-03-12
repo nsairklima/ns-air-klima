098468ad2566d51b8167efb981f2659c3d1f4bb5
nsairklima <nsair.klima@gmail.com>
2026-03-12 13:24:08 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone"
+  experimental: {
+    // Nagyon fontos: a pdfkit maradjon "külső" csomag, hogy a saját AFM fájljai elérhetők legyenek
+    serverComponentsExternalPackages: ["pdfkit"]
+  }
 };
 
 module.exports = nextConfig;
