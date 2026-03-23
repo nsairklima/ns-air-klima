b9a32d31fdd6b739b465f3888da5375f9e294826
nsairklima <nsair.klima@gmail.com>
2026-03-23 15:26:06 +0100

    Update route.ts

--- app/api/clients/[id]/units/[unitId]/route.ts
+++ app/api/clients/[id]/units/[unitId]/route.ts
@@ -23,3 +23,16 @@ export async function GET(
     return NextResponse.json({ error: "Szerver hiba" }, { status: 500 });
   }
 }
+export async function DELETE(
+  req: Request,
+  { params }: { params: { id: string; unitId: string } }
+) {
+  try {
+    await prisma.clientUnit.delete({
+      where: { id: Number(params.unitId) },
+    });
+    return NextResponse.json({ message: "Gép törölve" });
+  } catch (error) {
+    return NextResponse.json({ error: "Hiba a törléskor" }, { status: 500 });
+  }
+}
