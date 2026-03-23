3728d848f810351bfdd4fe3e444667c5b593dd1d
nsairklima <nsair.klima@gmail.com>
2026-03-23 12:58:16 +0100

    Delete app/api/clients/[id] directory

--- app/api/clients/[id]/route.ts
+++
@@ -1,8 +0,0 @@
-// app/api/clients/[id]/route.ts - Részlet
-const client = await prisma.client.findUnique({
-  where: { id: Number(params.id) },
-  include: {
-    units: true,  // <--- EZ NAGYON FONTOS!
-    quotes: true,
-  },
-});
