32af34b81c2f6c6b21f3a1502ec85979ee65269e
nsairklima <nsair.klima@gmail.com>
2026-04-01 09:19:40 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -82,7 +82,7 @@ export default function Dashboard() {
             boxShadow: "0 1px 2px rgba(0,0,0,0.05)"
           }}
         >
-          🛡️ Mentés
+          🛡️ Biztonsági mentés
         </button>
       </div>
 
