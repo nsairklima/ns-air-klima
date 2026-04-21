8dce4ed0116cbf7b0ffc19e6f81d893da8665c07
nsairklima <nsair.klima@gmail.com>
2026-04-21 13:38:36 +0200

    Update route.ts

--- app/api/admin/backup/route.ts
+++ app/api/admin/backup/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from 'next/server';
 import { prisma } from "@/lib/prisma";
 import { Resend } from "resend";
 
+export const dynamic = 'force-dynamic';
+
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 // Átírva GET-ről POST-ra, hogy megakadályozzuk a böngésző általi duplikált hívásokat
