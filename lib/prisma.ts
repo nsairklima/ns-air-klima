bd70ecbb06a7c933df07ea16f51c1de94fd5c226
nsairklima <nsair.klima@gmail.com>
2026-04-25 11:44:48 +0200

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,12 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    log: ["query"],
+    // Kikapcsolhatod a logolást productionben, ha zavar
+    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
   });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
