b94bd0b61d11e0fc71bcfcfde5559ead82f82c94
nsairklima <nsair.klima@gmail.com>
2026-04-29 13:13:03 +0200

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,7 +2,6 @@ import { PrismaClient } from '@prisma/client';
 
 const prismaClientSingleton = () => {
   return new PrismaClient({
-    log: ['error', 'warn'],
     datasources: {
       db: {
         url: process.env.DATABASE_URL,
