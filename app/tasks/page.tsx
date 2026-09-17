b82a2d6eb32c1f94f6c64a01fd00d8796383d432
nsairklima <nsair.klima@gmail.com>
2026-09-17 13:04:55 +0200

    Update page.tsx

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -528,11 +528,43 @@ const handleAddPhoto = async (
     e.preventDefault();
     setLoading(true);
     setStatusMessage("");
+let latitude = "";
+let longitude = "";
 
+if (address.trim()) {
+  try {
+    const coordinates =
+      await findCoordinates(address);
+
+    if (coordinates) {
+      latitude =
+        coordinates.lat.toString();
+
+      longitude =
+        coordinates.lng.toString();
+    }
+  } catch (error) {
+    console.error(
+      "Koordináta lekérési hiba:",
+      error
+    );
+  }
+}
     const formData = new FormData();
     formData.append("type", type);
     formData.append("name", name);
     formData.append("address", address);
+formData.append(
+  "latitude",
+  latitude
+);
+
+formData.append(
+  "longitude",
+  longitude
+);
+
+    
     formData.append("phone", phone);
     formData.append("email", email);
     formData.append("note", note);
